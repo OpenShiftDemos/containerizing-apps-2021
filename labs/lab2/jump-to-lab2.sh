841ba74c8e19b897d39a83951dc171e323a7b944
langdon <langdon@fedoraproject.org>
2021-07-30 19:42:16 -0400

    fixing up the playbooks to not try to engage crc

--- labs/lab2/jump-to-lab2.sh
+++ labs/lab2/jump-to-lab2.sh
@@ -4,6 +4,7 @@ read -p "This script is to jump over the previous labs, is that really what you 
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    sudo subscription-manager repos --enable=ansible-2-for-rhel-8-x86_64-rpms
     sudo yum install -y ansible
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     ansible-playbook -i "localhost," -c local $DIR/jump-to-here-playbook.yml
